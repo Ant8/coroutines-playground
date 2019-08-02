c98ccf7ee726fd29c92c02664eac03b2af4d955f
Antoni Moroz <antek@codequest.com>
2019-08-02 17:30:49 +0200

    results with time diff

--- app/src/main/java/com/abm/ant8/coroutinesplayground/Results.kt
+++ app/src/main/java/com/abm/ant8/coroutinesplayground/Results.kt
@@ -3,7 +3,7 @@ package com.abm.ant8.coroutinesplayground
 inline class RallyResult(val topTen: List<CrewResult>)
 inline class DriverChampionshipsStandings(val topTen: List<DriverResult>)
 
-data class CrewResult(val driver: Driver, val codriver: Codriver)
+data class CrewResult(val driver: Driver, val codriver: Codriver, val difference: String = "0.0")
 data class DriverResult(val driver: Driver, val points: Int)
 
 inline class Driver(val name: String)
