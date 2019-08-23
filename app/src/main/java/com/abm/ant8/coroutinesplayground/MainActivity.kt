4671f9b34fa301117a8bfbdde84f3fa556bd23b5
Antoni Moroz <antek@codequest.com>
2019-08-23 16:21:47 +0200

    basic ui, not connected - year dropdown

--- app/src/main/java/com/abm/ant8/coroutinesplayground/MainActivity.kt
+++ app/src/main/java/com/abm/ant8/coroutinesplayground/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.abm.ant8.coroutinesplayground
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        yearSelectorSpinner.adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.wrc_seasons,
+            android.R.layout.simple_spinner_item
+        ).apply {
+            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
     }
 }
