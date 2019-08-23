5aec6b2122c3ea475a9cd008b3b3b405b5a69462
Antoni Moroz <antek@codequest.com>
2019-08-23 18:21:53 +0200

    basic ui just resumes after async coroutine

---
+++ app/src/main/java/com/abm/ant8/coroutinesplayground/Constants.kt
@@ -0,0 +1,4 @@
+package com.abm.ant8.coroutinesplayground
+
+const val firstSeason = 1973
+const val lastSeason = 2019
--- app/src/main/java/com/abm/ant8/coroutinesplayground/MainActivity.kt
+++ app/src/main/java/com/abm/ant8/coroutinesplayground/MainActivity.kt
@@ -1,11 +1,35 @@
 package com.abm.ant8.coroutinesplayground
 
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import biz.kasual.materialnumberpicker.MaterialNumberPicker
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    private var driverChampionshipsStandings = DriverChampionshipsStandings(emptyList())
+
+    private val numberPicker by lazy {
+        MaterialNumberPicker.Builder(this)
+            .minValue(firstSeason)
+            .maxValue(lastSeason)
+            .defaultValue(lastSeason)
+            .textSize(20f)
+            .build()
+    }
+
+    private val dialog by lazy {
+        AlertDialog.Builder(this)
+            .setTitle("select WRC season")
+            .setView(numberPicker)
+            .setPositiveButton("OK") { _, _ -> onSeasonSelected(numberPicker.value) }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,13 +38,24 @@ class MainActivity : AppCompatActivity() {
         setupViews()
     }
 
+    private fun onSeasonSelected(season: Int) {
+        progressBar.visibility = VISIBLE
+        selectSeasonButton.text = season.toString()
+        GlobalScope.launch(Dispatchers.Main) {
+            driverChampionshipsStandings = withContext(Dispatchers.IO) {
+                WrcResultsRepository.getTopTenDriversOverallFor(season)
+            }
+            println(driverChampionshipsStandings)
+            progressBar.visibility = GONE
+        }
+    }
+
     private fun setupViews() {
-        yearSelectorSpinner.adapter = ArrayAdapter.createFromResource(
-            this,
-            R.array.wrc_seasons,
-            android.R.layout.simple_spinner_item
-        ).apply {
-            setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        selectSeasonButton.apply {
+            text = lastSeason.toString()
+            setOnClickListener {
+                dialog.show()
+            }
         }
     }
 }
