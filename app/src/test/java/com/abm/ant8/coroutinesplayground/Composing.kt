f05d05518a7c32b0bb9b6d66087c7dd8bfd3735d
Antoni Moroz <antek@codequest.com>
2019-08-30 16:31:54 +0200

    coroutines basics again - composing

--- app/src/test/java/com/abm/ant8/coroutinesplayground/Composing.kt
+++ app/src/test/java/com/abm/ant8/coroutinesplayground/Composing.kt
@@ -1,7 +1,6 @@
 package com.abm.ant8.coroutinesplayground
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 import org.junit.Test
 import kotlin.time.ExperimentalTime
 import kotlin.time.measureTimedValue
@@ -16,13 +15,111 @@ class Composing {
         println("completion time ${timedValue.duration}, result ${timedValue.value}")
     }
 
-    suspend fun doSomethingUsefulOne(): Int {
+    @ExperimentalTime
+    @Test
+    fun runInParallel() = runBlocking {
+        val timedValue = measureTimedValue {
+            val one = async {
+                doSomethingUsefulOne()
+            }
+            val two = async {
+                doSomethingUsefulTwo()
+            }
+            one.await() + two.await()
+        }
+        println("completion time ${timedValue.duration}, result ${timedValue.value}")
+    }
+
+    @ExperimentalTime
+    @Test
+    fun startLazy() = runBlocking {
+        val timedValue = measureTimedValue {
+            val one = async(start = CoroutineStart.LAZY) {
+                doSomethingUsefulOne()
+            }
+            val two = async(start = CoroutineStart.LAZY) {
+                doSomethingUsefulTwo()
+            }
+            one.start()
+            two.start()
+            one.await()
+        }
+        println("completion time ${timedValue.duration}, result ${timedValue.value}")
+    }
+
+    @ExperimentalTime
+    @Test
+    fun startDefault() = runBlocking {
+        val timedValue = measureTimedValue {
+            val one = async(start = CoroutineStart.DEFAULT) {
+                doSomethingUsefulOne()
+            }
+            val two = async(start = CoroutineStart.DEFAULT) {
+                doSomethingUsefulTwo()
+            }
+            one.start()
+            two.start()
+            one.await()
+        }
+        println("completion time ${timedValue.duration}, result ${timedValue.value}")
+    }
+
+    @ExperimentalTime
+    @Test
+    fun discouragedAsyncStyle() {
+        val timedValue = measureTimedValue {
+            val one = doSomethingUsefulOneAsync()
+            val two = doSomethingUsefulTwoAsync()
+
+            runBlocking {
+                println("result ${one.await() + two.await()}")
+            }
+        }
+        println("completion time ${timedValue.duration}")
+    }
+
+    @ExperimentalTime
+    @Test
+    fun structuredConcurrency() = runBlocking {
+        val timedValue = measureTimedValue {
+            var i = 0
+            try {
+                withTimeout(100) {
+                    i = concurrentSum()
+                }
+            } catch (e: Exception) {
+                print(e)
+            }
+            return@measureTimedValue i
+        }
+        println("${timedValue.duration}, ${timedValue.value}")
+    }
+
+    private suspend fun concurrentSum(): Int = coroutineScope {
+        val one = async {
+            doSomethingUsefulOne()
+        }
+        val two = async {
+            doSomethingUsefulTwo()
+        }
+        one.await() + two.await()
+    }
+
+    private fun doSomethingUsefulOneAsync(): Deferred<Int> = GlobalScope.async {
+        doSomethingUsefulOne()
+    }
+
+    private fun doSomethingUsefulTwoAsync(): Deferred<Int> = GlobalScope.async {
+        doSomethingUsefulTwo()
+    }
+
+    private suspend fun doSomethingUsefulOne(): Int {
         delay(1000L) // pretend we are doing something useful here
         return 13
     }
 
-    suspend fun doSomethingUsefulTwo(): Int {
-        delay(1000L) // pretend we are doing something useful here, too
+    private suspend fun doSomethingUsefulTwo(): Int {
+        delay(1500L) // pretend we are doing something useful here, too
         return 29
     }
 }
