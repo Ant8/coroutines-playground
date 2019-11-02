cef1e62e7e81ba012293332c214831200166938e
Antoni Moroz <antek@codequest.com>
2019-11-02 16:59:01 +0100

    season list fragment set up

--- app/src/main/java/com/abm/ant8/coroutinesplayground/view/MainActivity.kt
+++ app/src/main/java/com/abm/ant8/coroutinesplayground/view/MainActivity.kt
@@ -1,12 +1,17 @@
-package com.abm.ant8.coroutinesplayground
+package com.abm.ant8.coroutinesplayground.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abm.ant8.coroutinesplayground.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.root, SeasonListFragment.newInstance())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/abm/ant8/coroutinesplayground/view/SeasonListViewModel.kt
@@ -0,0 +1,7 @@
+package com.abm.ant8.coroutinesplayground.view
+
+import androidx.lifecycle.ViewModel
+
+class SeasonListViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
